01580c63af5f20ab2ca4d5d46c1389da0cad7bc8
Mazhar <mazharulnihadmd@gmail.com>
2025-08-15 23:29:49 +0600

    feat (handler) : add error control, GET method checking, cors, status code

--- internal/adpter/http/handler/product_handler.go
+++ internal/adpter/http/handler/product_handler.go
@@ -1,22 +1,34 @@
 package handler
 
 import (
-    "net/http"
-    "encoding/json"
-    "github/ecommerce/internal/usecase"
+	"encoding/json"
+	"github/ecommerce/internal/usecase"
+	"net/http"
 )
 
 type ProductHandler struct {
-    Service *usecase.ProductService
+	Service *usecase.ProductService
 }
 
 // Constructor
 func NewProductHandler(service *usecase.ProductService) *ProductHandler {
-    return &ProductHandler{Service: service}
+	return &ProductHandler{Service: service}
 }
 
 // Handler method
 func (h *ProductHandler) GetProducts(w http.ResponseWriter, r *http.Request) {
-    products, _ := h.Service.GetAllProducts()
-    json.NewEncoder(w).Encode(products)
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+	w.Header().Set("Content-Type", "application/json")
+
+	if r.Method != "GET" {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	products, err := h.Service.GetAllProducts()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	json.NewEncoder(w).Encode(products)
 }
