4389f9ba48a65ced09d11923c986761ac9ea00ee
Mazhar <mazharulnihadmd@gmail.com>
2025-08-16 23:55:06 +0600

    feat (handler) : add CreateProduct

--- internal/adpter/http/handler/product_handler.go
+++ internal/adpter/http/handler/product_handler.go
@@ -2,6 +2,7 @@ package handler
 
 import (
 	"encoding/json"
+	"github/ecommerce/internal/domain/product"
 	"github/ecommerce/internal/usecase"
 	"net/http"
 	"strconv"
@@ -68,6 +69,22 @@ func (h *ProductHandler) CreateProduct(w http.ResponseWriter, r *http.Request) {
 
 	if r.Method != "POST" {
 		http.Error(w, "Only post is allowed", http.StatusBadRequest)
+		return
 	}
+
+	var prod product.Product
+	decoder := json.NewDecoder(r.Body)
+	err := decoder.Decode(&prod)
+	if err != nil {
+		http.Error(w, "invalid request", http.StatusBadRequest)
+		return
+	}
+	err = h.Service.InsertProduct(prod)
+	if err != nil {
+		http.Error(w, "Internal server error", 500)
+		return
+	}
+	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte("Resource created successfully"))
 
 }
