66c93baca8178047b3663bdb621b9ce495c826bc
Mazhar <mazharulnihadmd@gmail.com>
2025-08-17 22:18:48 +0600

    feat : add Description

--- internal/domain/product/product.go
+++ internal/domain/product/product.go
@@ -1,9 +1,10 @@
 package product
 
 type Product struct {
-	Id     int    `json:"id"`
-	Name   string `json:"name"`
-	Type   string `json:"type"`
-	Price  int    `json:"price"`
-	ImgUrl string `json:"img_url"`
+	Id          int     `json:"id"`
+	Name        string  `json:"name"`
+	Description string  `json: "description"`
+	Type        string  `json:"type"`
+	Price       float64 `json:"price"`
+	ImgUrl      string  `json:"img_url"`
 }
