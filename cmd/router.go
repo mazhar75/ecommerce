436928d5f7cac4cd1549b593ec6efa8760d0ce5c
Mazhar <mazharulnihadmd@gmail.com>
2025-08-24 18:38:53 +0600

    feat : add logger middleware

--- cmd/router.go
+++ cmd/router.go
@@ -2,11 +2,17 @@ package cmd
 
 import (
 	"github/ecommerce/adapter/handlers/product_handlers"
+	"github/ecommerce/middlewares"
 	"net/http"
 )
 
 func RegisterRoutes(mux *http.ServeMux, productHandler *product_handlers.ProductHandler) {
-	mux.Handle("GET /products", http.HandlerFunc(productHandler.GetProducts))
+	h := http.HandlerFunc(productHandler.GetProducts)
+	h1 := middlewares.MiddlwareTest1(h)
+	h2 := middlewares.MiddlewareTest2(h1)
+	h3 := middlewares.Logger(h2)
+
+	mux.Handle("GET /products", h3)
 	mux.Handle("GET /products/{productId}", http.HandlerFunc(productHandler.GetProductById))
 	mux.Handle("POST /products", http.HandlerFunc(productHandler.CreateProduct))
 }
