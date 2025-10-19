85c555031b0cf45ec0cc82f46f529dc95faac805
Mazhar <mazharulnihadmd@gmail.com>
2025-10-19 23:14:38 +0600

    feat: cart service added

---
+++ usecase/cart_service.go
@@ -0,0 +1,14 @@
+package usecase
+
+import "github/ecommerce/domain/cart"
+
+type CartService struct {
+	Repo cart.CartRepository
+}
+
+func NewCartService(repo cart.CartRepository) *CartService {
+	return &CartService{Repo: repo}
+}
+func (s *CartService) GetCartByUserId(user_id int) cart.Carts {
+	return s.Repo.GetCartByUserId(user_id)
+}
