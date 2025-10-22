b0c75302a01617077358781faffbb35dbc403d21
Mazhar <mazharulnihadmd@gmail.com>
2025-10-22 21:45:04 +0600

    feat: add all routes in handler

--- adapter/handlers/cart_item_handler/handler.go
+++ adapter/handlers/cart_item_handler/handler.go
@@ -15,4 +15,18 @@ func NewCartItemHandler(service *usecase.CartItemService) *CartItemHandler {
 }
 func (h *CartItemHandler) RegisterRoutes(mux *http.ServeMux, manager *middlewares.Manager) {
 
+	mux.Handle("POST /cart/{user_id}/add", manager.With(
+		http.HandlerFunc(h.AddProductToCart),
+		middlewares.AuthMiddleware,
+	))
+
+	mux.Handle("PATCH /cart/{user_id}/update", manager.With(
+		http.HandlerFunc(h.UpdateQuantity),
+		middlewares.AuthMiddleware,
+	))
+	mux.Handle("DELETE /cart/{user_id}/remove/{product_id}",
+		manager.With(
+			http.HandlerFunc(h.DeleteFromCart),
+			middlewares.AuthMiddleware,
+		))
 }
