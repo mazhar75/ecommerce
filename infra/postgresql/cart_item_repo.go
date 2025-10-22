b7fe9ae55bcc33fc42ce5464aea3975882065826
Mazhar <mazharulnihadmd@gmail.com>
2025-10-22 22:27:26 +0600

    fix: query insert into cart_item(cart_id,product_id,quantity) values(,,) returning cart_item_id

--- infra/postgresql/cart_item_repo.go
+++ infra/postgresql/cart_item_repo.go
@@ -22,19 +22,21 @@ func (r *CartItemRepo) AddProductToCart(user_id int, product_id int) error {
              from cart_item 
 			 join cart on cart_item.cart_id=cart.cart_id 
 			 where cart.user_id=$1 and cart_item.product_id=$2`
-	err := db.QueryRow(query, user_id, product_id).Scan(&cartItemId, &quantity)
+	err := r.DB.QueryRow(query, user_id, product_id).Scan(&cartItemId, &quantity)
 	if err == sql.ErrNoRows {
 		query = `select cart_id from cart where user_id=$1`
 		var cartId, cartItemId int
-		err = db.QueryRow(query, user_id).Scan(&cartId)
+		err = r.DB.QueryRow(query, user_id).Scan(&cartId)
 		if err != nil {
+			fmt.Println("Here error 1")
 			fmt.Println(err)
 			return err
 		}
 		quantity := 1
-		query = `insert into cart_items(cart_id,product_id,quantity) values($1,$2,$3)`
-		err = db.QueryRow(query, cartId, product_id, quantity).Scan(&cartItemId)
+		query = `insert into cart_item(cart_id,product_id,quantity) values($1,$2,$3) returning cart_item_id`
+		err = r.DB.QueryRow(query, cartId, product_id, quantity).Scan(&cartItemId)
 		if err != nil {
+			fmt.Println("Here error 2")
 			fmt.Println(err)
 			return err
 		}
@@ -42,11 +44,14 @@ func (r *CartItemRepo) AddProductToCart(user_id int, product_id int) error {
 		return nil
 
 	} else if err != nil {
+		fmt.Println("Here error 3")
 		return err
 	} else {
-		query = `update cart_item set quanity=$1 where cart_item_id=$2`
-		_, err = db.Exec(query, quantity, cartItemId)
+		query = `update cart_item set quantity=$1 where cart_item_id=$2`
+		quantity++
+		_, err = r.DB.Exec(query, quantity, cartItemId)
 		if err != nil {
+			fmt.Println("Here error 4")
 			fmt.Println(err)
 			return err
 		}
@@ -57,13 +62,13 @@ func (r *CartItemRepo) AddProductToCart(user_id int, product_id int) error {
 func (r *CartItemRepo) ChangeQuantity(user_id int, product_id int, quantity int) error {
 	query := `select cart_id from cart where user_id=$1`
 	var cartId int
-	err := db.QueryRow(query, user_id).Scan(&cartId)
+	err := r.DB.QueryRow(query, user_id).Scan(&cartId)
 	if err != nil {
 		fmt.Println(err)
 		return err
 	}
 	query = `update cart_item set quantity=$1 where cart_id=$2 and product_id=$3`
-	_, err = db.Exec(query, quantity, cartId, product_id)
+	_, err = r.DB.Exec(query, quantity, cartId, product_id)
 	if err != nil {
 		fmt.Println(err)
 		return err
@@ -74,13 +79,13 @@ func (r *CartItemRepo) ChangeQuantity(user_id int, product_id int, quantity int)
 func (r *CartItemRepo) DeleteProductFromCart(user_id int, product_id int) error {
 	query := `select cart_id from cart where user_id=$1`
 	var cartId int
-	err := db.QueryRow(query, user_id).Scan(&cartId)
+	err := r.DB.QueryRow(query, user_id).Scan(&cartId)
 	if err != nil {
 		fmt.Println(err)
 		return err
 	}
 	query = `delete from cart_item where cart_id=$1 and product_id=$2`
-	_, err = db.Exec(query, cartId, product_id)
+	_, err = r.DB.Exec(query, cartId, product_id)
 	if err != nil {
 		fmt.Println(err)
 		return err
