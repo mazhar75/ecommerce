0160b26e5f9eca4ae06d76d24289a653fd010699
Mazhar <mazharulnihadmd@gmail.com>
2025-10-23 23:33:15 +0600

    fix: changed cart model and repo, add selection option

--- domain/cart/cart.go
+++ domain/cart/cart.go
@@ -5,10 +5,11 @@ type Carts struct {
 	UserID int `json:"user_id"`
 }
 type CartItems struct {
-	CartItemsId int `json:"cartitems_id"`
-	CartId      int `json:"cart_id"`
-	ProductId   int `json:"product_id"`
-	Quantity    int `json:"quantity"`
+	CartItemsId int  `json:"cartitems_id"`
+	CartId      int  `json:"cart_id"`
+	ProductId   int  `json:"product_id"`
+	Quantity    int  `json:"quantity"`
+	IsSelected  bool `json:"is_selected"`
 }
 
 type CartRepository interface {
