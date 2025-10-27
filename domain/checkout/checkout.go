d763218dacaf79279a65376feb0ae27d5be8bdd9
Mazhar <mazharulnihadmd@gmail.com>
2025-10-27 23:03:07 +0600

    refactor(checkout): add final json format for redis storing

--- domain/checkout/checkout.go
+++ domain/checkout/checkout.go
@@ -1,5 +1,7 @@
 package checkout
 
+import "time"
+
 type Checkout struct {
 	ProductId int    `json:"product_id"`
 	Name      string `json:"name"`
@@ -11,9 +13,16 @@ type OutOfStock struct {
 	ProductId int    `json:"product_id"`
 	Name      string `json:"name"`
 }
+
+type CheckoutFinal struct {
+	CartId      int        `json:"cart_id"`
+	ProductList []Checkout `json:"product_list"`
+	TotalPrice  float64    `json:"total_price"`
+	ExpireAt    time.Time  `json:"expire_at"`
+}
 type CheckoutPgRepository interface {
 	GetAllProductsFromCart(cart_id int) ([]Checkout, []OutOfStock, error)
 }
 type CheckoutRedisRepository interface {
-	InsertAllProductsIntoRedis(products []Checkout) error
+	InsertAllProductsIntoRedis(products []Checkout, cart_id int) error
 }
